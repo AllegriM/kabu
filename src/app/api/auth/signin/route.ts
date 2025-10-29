ba5973a811580234adbbfb9548ef8c4a048d6246
AllegriM <elmonky99@gmail.com>
2025-10-29 14:19:28 -0300

    fix url login

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -1,13 +1,26 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
 
 export async function POST() {
   try {
     const supabase = await createClient();
+
+    const origin = (await headers()).get("origin");
+
+    if (!origin) {
+      return NextResponse.json(
+        { error: "Missing origin header" },
+        { status: 400 }
+      );
+    }
+
+    const redirectTo = `${origin}/auth/callback`;
+
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `http://localhost:3000/auth/callback`,
+        redirectTo: redirectTo,
       },
     });
     if (error) {
